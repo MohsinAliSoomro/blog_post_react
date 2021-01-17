f5fa8b26d7c1071f917c37b7222bc4b6382b8597
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2021-01-17 15:03:30 +0500

    whatwg-fetch install

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import Post from './components/post';
 // import Video from './components/video';
 // import axios from 'axios';
-import { fetch } from 'whatwg-fetch';
+// import { fetch } from 'whatwg-fetch';
 function App() {
 	const [ posts, setPosts ] = useState([]);
 	// const [ loadMore, setLoadMore ] = useState(1);
 	useEffect(() => {
-		fetch('https://www.spatialardhi.com/estate/posts/?format=json').then((res) => res.json()).then((data) => {
-			if (data) {
-				console.log(data);
-				setPosts(data);
-			}
-		});
+		async function GetData() {
+			const json = await fetch('https://www.spatialardhi.com/estate/posts/?format=json');
+			const data =await json.json();
+			setPosts(data)
+		}
+		// fetch('https://www.spatialardhi.com/estate/posts/?format=json').then((res) => res.json()).then((data) => {
+		// 	if (data) {
+		// 		console.log(data);
+		// 		setPosts(data);
+		// 	}
+		// });
+		GetData();
 	}, []);
 	return (
 		<div className="container" style={{ backgroundColor: 'magenta' }}>
