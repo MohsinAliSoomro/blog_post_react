d0d6cc76f3b50388839bb1c799e312eaa90bac31
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2021-01-17 15:24:15 +0500

    whatwg-fetch install

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Post from './components/post';
 // import axios from 'axios';
 // import { fetch } from 'whatwg-fetch';
 function App() {
-	const [ posts, setPosts ] = useState([]);
+	const [ posts, setPosts ] = useState({loading:true,Data:[]});
 	// const [ loadMore, setLoadMore ] = useState(1);
 	useEffect(() => {
+		setPosts({loading:true,Data:[]})
 		async function GetData() {
 			const json = await fetch('http://www.spatialardhi.com/estate/posts/?format=json', {
 				method: 'GET'
 			});
 			const data = await json.json();
-			setPosts(data);
+			setPosts({loading:false,Data:data});
 		}
 		// fetch('https://www.spatialardhi.com/estate/posts/?format=json').then((res) => res.json()).then((data) => {
 		// 	if (data) {
@@ -23,13 +24,16 @@ function App() {
 		// });
 		GetData();
 	}, []);
+	if (posts.loading) {
+		return (<div>Loading...</div>)
+	}
 	return (
 		<div className="container" style={{ backgroundColor: 'magenta' }}>
 			<h2 style={{ color: 'cyan' }}>Blog Posts</h2>
 			<hr />
 			<div className="row">
 				<div className="col">
-					{posts.map((post) => {
+					{posts.Data.map((post) => {
 						return <Post post={post} key={post.id} />;
 					})}
 				</div>
